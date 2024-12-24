38f528e466b8e7f197cb169d918d706c2e745788
Atakku <atakkudev@gmail.com>
2024-12-24 13:32:20 +0100

    Cleanup a little

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
-#syntax=docker/dockerfile:1.3-labs
-FROM docker.io/rustlang/rust:nightly-slim as builder
+FROM docker.io/rustlang/rust:slim as builder
 WORKDIR /build
 RUN apt-get update && apt-get install -y pkg-config libssl-dev && rm -rf /var/lib/apt/lists/*
 # Cache dependencies by making a fake project
 COPY Cargo.toml Cargo.toml
-RUN <<EOF
-mkdir src && touch src/main.rs
-cargo build --release
-rm -rf src Cargo.toml
-EOF
+RUN mkdir src && touch src/lib.rs && cargo build --release && rm -rf src
+# Actually copy the project and build it
 COPY . .
 RUN cargo build --release
-FROM docker.io/rustlang/rust:nightly-slim
+# Deploy the project
+FROM docker.io/rustlang/rust:slim
 WORKDIR /app
 RUN apt-get update && apt-get install -y ca-certificates libssl3 openssl && rm -rf /var/lib/apt/lists/*
 COPY --from=builder /build/target/release/s2fa /app/s2fa
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-// Copyright 2023 Atakku <https://atakku.dev>
+// Copyright 2025 Atakku <https://atakku.dev>
 //
 // This project is dual licensed under MIT and Apache.
 
 use actix_web::{get, App, HttpServer};
-
 use steam_totp::{generate_auth_code, Secret, Time};
 
 pub type Res<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[get("/")]
 async fn gen_code() -> Res<String> {
-    let time = Time::with_offset().await?;
-    let secret = Secret::from_b64(&std::env::var("SECRET")?)?;
-    Ok(generate_auth_code(secret, time))
+  let time = Time::with_offset().await?;
+  let secret = Secret::from_b64(&std::env::var("SECRET")?)?;
+  Ok(generate_auth_code(secret, time))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(gen_code))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+  HttpServer::new(|| App::new().service(gen_code)).bind(("0.0.0.0", 8080))?.run().await
 }
